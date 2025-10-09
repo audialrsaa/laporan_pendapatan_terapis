3b3e2349bd7173b4f7b60c52cd101aa322a8dbb1
audialrsaa <araspxr@gmail.com>
2025-10-09 11:05:09 +0700

    sec commit

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,11 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  output: 'export', // penting untuk GitHub Pages
+  images: {
+    unoptimized: true,
+  },
+  basePath: '/laporan_pendapatan_terapis', // sesuaikan dengan nama repo kamu
+  assetPrefix: '/laporan_pendapatan_terapis/',
+};
 
 export default nextConfig;
